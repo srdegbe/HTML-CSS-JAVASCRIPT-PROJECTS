86caaf50cd49213cd4421d4deb324d677e009c33
Raymond Degbe <srdegbe14@gmail.com>
2023-12-26 08:20:57 +0000

    Init: Social book project implementation

---
+++ socialbook/script.js
@@ -0,0 +1,33 @@
+const settingsMenu = document.querySelector(".settings-menu");
+const darkBtn = document.querySelector("#dark-btn");
+const pageBody = document.querySelector("body");
+
+const theme = localStorage.getItem("theme");
+
+function menuDropdownToggle(){
+   console.log("Active")
+   settingsMenu.classList.toggle("settings-menu-height");
+}
+
+darkBtn.onclick = function darkModeToggleFx(){
+
+   darkBtn.classList.toggle("dark-btn-on");
+   pageBody.classList.toggle("dark-theme");
+
+   if(theme === "dark"){
+      localStorage.setItem("theme", "light");
+   } else {
+      localStorage.setItem("theme", "dark");
+   }
+
+}
+
+if(theme === "light"){
+   darkBtn.classList.remove("dark-btn-on");
+   pageBody.classList.remove("dark-theme")
+} else if(theme === "dark"){
+   darkBtn.classList.add("dark-btn-on");
+   pageBody.classList.add("dark-theme")
+} else {
+   localStorage.setItem("theme", "light");
+}
